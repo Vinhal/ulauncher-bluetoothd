a855df59f4c95332ee4bb5729d20780e5e0c7466
Lucas Vinhal <lucvinhal@gmail.com>
2022-12-14 21:49:07 -0300

    showing icon name on option

--- main.py
+++ main.py
@@ -47,7 +47,7 @@ class KeywordQueryEventListener(EventListener):
             icon_name = "{}_{}".format(device["icon"], device["active"])
             icon_path = 'images/{}.png'.format(icon_name)
             description = description_active if device["active"] else description_inactive
-            description = description.format(device["uuid"] + "icon_name")
+            description = description.format(device["uuid"] + icon_name)
 
             if not os.path.isfile(extension_home + "/" + icon_path):
                 logger.warning("Icon not found: " + icon_path)
